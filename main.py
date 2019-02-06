c816d56479fd2745331f9da252377664e6d60e78
miguelsd0501 <miguelsd0501@gmail.com>
2019-02-05 23:28:27 -0600

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ def sum(a, b):
 
 @app.route("/")
 def hello():
-    sum = sum(5, 2)
-    return "Hello World!: %s" % sum
+    res = sum(5, 2)
+    return "Hello World!: %s" % (res)
 
 if __name__ == "__main__":
     app.run(host = '0.0.0.0', port = 5000)
